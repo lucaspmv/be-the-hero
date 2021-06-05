6baf190eec54bba16a124fe2c2f353abd859e687
Lucas Porto <lucaspmv@hotmail.com>
2021-06-05 15:56:31 -0300

    Consumo de api no front com axios e frontend finalizado.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// JSX (JavaScript + XML)
+/**
+ *  Componente é uma função que retorna HTML.
+ *  Propriedades são atributos de componentes.
+ * */
+
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
