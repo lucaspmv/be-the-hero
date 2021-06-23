fd809e4920a7c8e9c37ccc03a2369b7f8c10f4d2
Lucas Porto <lucaspmv@hotmail.com>
2021-06-22 23:50:44 -0300

    Funcionalidades avançadas: aplicando validações nas rotas de post,get e delete do node, e aplicando testes unitários e de integração.

--- backend/src/app.js
+++ backend/src/app.js
@@ -36,6 +36,7 @@
 
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -43,5 +44,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
